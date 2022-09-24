7e428c2f7d5ad328b997fbe637d2834fa1ab0089
Aleksandr <MrRotod@gmail.com>
2022-09-24 22:28:29 +0500

    решил первую задачу

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿/// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int GetNumber()
+{
+    Console.Write("Введите пятизначное число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool RazrNumber (int number) /// метод разрядности числа и сравнения их
+{
+    int DecT = (number/10000)%10;
+    int tic = (number/1000)%10;
+    int Sot = (number/100)%10;
+    int Dec = (number/10)%10;
+    int Ed = number%10;
+    if (DecT == Ed && tic == Dec)
+    return true;
+    else return false;
+                
+}
+int a = GetNumber();
+bool b = RazrNumber(a);
+if (b == true)
+    Console.WriteLine("Число  " + a + "  является палиндромом");
+else
+    Console.WriteLine("Число  " + a + "  не является палиндромом");
+
