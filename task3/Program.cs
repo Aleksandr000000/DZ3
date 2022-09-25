2719a25f14fde67727a1da8721c8d2d2c583f0a4
Aleksandr <MrRotod@gmail.com>
2022-09-25 11:53:31 +0500

    решил третью задачу

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int  GetNumber() /// метод ввода числа 
+{
+    Console.WriteLine("Введите число");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void GetPrintPow (int number) // метод возведения в  степень 
+
+{
+    int Count = 0;
+    Console.WriteLine("таблица кубов от 1 до " + number);
+    while (Count <= number)
+    {
+        Console.Write("->" + Math.Pow(Count,3)+" ");
+        Count ++;
+    }
+}
+GetPrintPow(GetNumber());
